b3428bc28894f2bf41e755939d7c983a8a93edd6
Maria Musalitina <patronussun@gmail.com>
2023-09-13 13:35:07 +0400

    update game scripts

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
-from brain_games.common import game_result
+from brain_games.common import game
 from brain_games.games.gcd_game import brain_gcd
 
 
 def main():
-    name = welcome_user()
-    result = brain_gcd(start_wins_count=1)
-    game_result(name, result)
+    game(brain_gcd)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.cli import welcome_user
-from brain_games.common import game_result
+from brain_games.common import game
 from brain_games.games.progression_game import brain_progression
 
 
 def main():
-    name = welcome_user()
-    result = brain_progression(start_wins_count=1)
-    game_result(name, result)
+    game(brain_progression)
 
 
 if __name__ == '__main__':
